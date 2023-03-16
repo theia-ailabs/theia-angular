c3b367e262c62b84ed6ec0b75cc6594d46122592
JRosSx91 <hazee.1491@gmail.com>
2023-03-16 18:02:22 +0100

    importing moduls on service.ts

--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AppService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getData(url: string): Observable<any> {
+    return this.http.get(url);
+  }
+
 }

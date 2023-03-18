845c348b88239eef2795df370d6a21985b841ea2
JRosSx91 <hazee.1491@gmail.com>
2023-03-18 14:46:08 +0100

    importing moduls new loader

--- src/app/components/loader/loader.component.ts
+++ src/app/components/loader/loader.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import * as THREE from 'three';
+import { EffectComposer } from 'three/examples/jsm/postprocessing/EffectComposer.js';
+import { RenderPass } from 'three/examples/jsm/postprocessing/RenderPass.js';
+import { UnrealBloomPass } from 'three/examples/jsm/postprocessing/UnrealBloomPass.js';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 
 @Component({
   selector: 'app-loader',

c38ac69005744e70a4b06fe6fd7e1ae109ec16a5
JRosSx91 <hazee.1491@gmail.com>
2023-03-22 10:45:10 +0100

    finishing loader

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,6 +14,6 @@ export class AppComponent implements OnInit {
   ngOnInit(): void {
     setTimeout(() => {
       this.loading = false;
-    }, 20000);
+    }, 35000);
   }
 }
--- src/app/components/text-loader/text-loader.component.ts
+++ src/app/components/text-loader/text-loader.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-text-loader',
   templateUrl: './text-loader.component.html',
   styleUrls: ['./text-loader.component.css']
 })
-export class TextLoaderComponent {
-
+export class TextLoaderComponent implements OnInit {
+  ngOnInit() {
+    let i = 1;
+    const intervalId = setInterval(() => {
+      const div = document.getElementById(`appear${i}`);
+      if (div) {
+        div.style.display = 'block';
+        i++;
+      } else {
+        clearInterval(intervalId);
+      }
+    }, 3000);
+  }
 }

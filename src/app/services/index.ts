77f3f72fd402f50535db4aa20b3197df9e5cb995
JRosSx91 <hazee.1491@gmail.com>
2023-03-27 17:02:28 +0200

    add store

--- src/app/services/index.ts
+++ src/app/services/index.ts
@@ -1,14 +1,19 @@
-import { HttpClient, HttpHandler } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
+import { Answer } from '../types';
 
 const SERVER_URL: string = 'https://theia-server_762122.onrender.com/theia';
 
-export const askTheia = async (question: Buffer | string): Promise<any> => {
+export const askTheia = async (question: Buffer | string):
+    Promise<Object | Answer> => {
     let http: HttpClient;
     let body = { 'question': question };
     if (!question) {
         body.question = 'welcome';
     }
     const json = JSON.stringify(body);
-    http.post<any>(SERVER_URL, json).subscribe((res) => console.log(res))
+    http.post<any>(SERVER_URL, json).subscribe((res) => {
+        console.log(res);
+        return res as Answer;
+    })
     return http.post(SERVER_URL, body);
 }

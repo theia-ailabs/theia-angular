72ad55eac6f4dbdec7449888b027f79abb8deff9
Julia Outumuro <juliaoutumuro@MacBook-Air.local>
2023-03-29 09:42:22 +0200

    add play btn to theia body

--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Message } from './types';
+import { VoiceRec } from './services/voiceRec';
+import { getDate, getTime } from './utils';
 
 @Injectable({
   providedIn: 'root',
@@ -8,6 +10,25 @@ export class AppService {
   messages: Message[] = [];
 
   constructor() {}
+
+  ngOnInit() {
+    const speechRec = new VoiceRec();
+    const audioMsg = speechRec.start();
+    this.voiceMsg(audioMsg);
+  }
+
+  voiceMsg(audioMsg: string): void {
+    const msg: Message = {
+      author: 'Recorder',
+      message: audioMsg,
+      datetime: getDate() + ' ' + getTime(),
+    };
+    this.messages.push(msg);
+  }
+
+  addMessage(message: Message) {
+    this.messages.push(message);
+  }
 
   init() {}
 }
--- src/app/components/message-area/message-area.component.ts
+++ src/app/components/message-area/message-area.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { AppService } from '../../app.service';
-import { VoiceRec } from '../../services/voiceRec';
 import { getDate, getTime } from '../../utils';
 import { Message } from '../../types';
 
@@ -8,43 +7,29 @@ import { Message } from '../../types';
   selector: 'app-message-area',
   templateUrl: './message-area.component.html',
   styleUrls: ['./message-area.component.css'],
-  providers: [AppService]
+  providers: [AppService],
 })
-
 export class MessageAreaComponent implements OnInit {
-
   userMessage: string;
   messages: Message[];
 
   constructor(public service: AppService) {
     this.messages = [];
-    this.userMessage = "";
+    this.userMessage = '';
   }
 
-  ngOnInit() {
-    const speechRec = new VoiceRec();
-    const audioMsg = speechRec.start();
-    this.voiceMsg(audioMsg);
-  }
-
-  voiceMsg(audioMsg: string): void {
-    const msg: Message = {
-      author: 'Recorder',
-      message: audioMsg,
-      datetime: getDate() + ' ' + getTime()
-    }
-    this.messages.push(msg);
+  ngOnInit(): void {
+    return;
   }
 
   inputMessage(): void {
     const msg: Message = {
       author: 'User',
       message: this.userMessage,
-      datetime: getDate() + ' ' + getTime()
-    }
+      datetime: getDate() + ' ' + getTime(),
+    };
     this.messages.push(msg);
     this.userMessage = '';
     console.log(this.messages);
   }
-
 }

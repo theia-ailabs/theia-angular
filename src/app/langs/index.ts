52a766975c8af5a06f2865552f90c5e540124db6
Julia Outumuro <juliaoutumuro@MacBook-Air.local>
2023-03-29 10:58:26 +0200

    add langs

---
+++ src/app/langs/index.ts
@@ -0,0 +1,10 @@
+export const langs = {
+  en: {
+    welcome: 'Welcome to Theia',
+    navbar: 'Welcome, ',
+  },
+  es: {
+    welcome: 'Bienvenido a Theia',
+    navbar: 'Bienvenido, ',
+  },
+};

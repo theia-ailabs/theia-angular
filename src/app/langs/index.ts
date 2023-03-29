df52fd5bd140ade59e215ac09df2222bf1619abf
Julia Outumuro <juliaoutumuro@MacBook-Air.local>
2023-03-29 11:35:04 +0200

    add langs

--- src/app/langs/index.ts
+++ src/app/langs/index.ts
@@ -3,10 +3,24 @@ export const langs = {
     welcome: 'Welcome to Theia',
     navbar: 'Welcome, ',
     input: '🗣️ Ask me anything...',
+    loader_logs: [
+      'Detecting your local language...',
+      'Idioma detectado: EN (United Kingdom)',
+      'Loading systems...',
+      'What is your name...?',
+      'Welcome, ',
+    ],
   },
   es: {
     welcome: 'Bienvenido a Theia',
     navbar: 'Bienvenido, ',
     input: '🗣️ Pregúntame cualquier cosa...',
+    loader_logs: [
+      'Detectando tu idioma local...',
+      'Idioma detectado: ES (España)',
+      'Cargando sistemas...',
+      'Cual es tu nombre...?',
+      'Bienvenido, ',
+    ],
   },
 };

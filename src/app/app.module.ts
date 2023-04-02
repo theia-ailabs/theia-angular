9d11ba44c75431ef167b47ae8a84fe83a2b9c7f5
Julia Outumuro <juliaoutumuro@MacBook-Air.local>
2023-04-02 16:43:57 +0200

    clean components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,37 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterOutlet } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
+import { AppService } from './app.service';
 import { AppComponent } from './app.component';
+import { WelcomeScreenComponent } from './components/welcome-screen/welcome-screen.component';
+import { BackgroundCanvasComponent } from './components/background-canvas/background-canvas.component';
+import { TextLoaderComponent } from './components/text-loader/text-loader.component';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 import { MainDisplayComponent } from './components/main-display/main-display.component';
-import { AiBodyComponent } from './components/ai-body/ai-body.component';
 import { MessageAreaComponent } from './components/message-area/message-area.component';
+import { AudioWaveComponent } from './components/audio-wave/audio-wave.component';
+import { AiBodyComponent } from './components/ai-body/ai-body.component';
+import { SvgIaComponent } from './components/ai-body/svg-ia/svg-ia.component';
+import { SvgTalkingComponent } from './components/ai-body/svg-talking/svg-talking.component';
 import { FooterBarComponent } from './components/footer-bar/footer-bar.component';
 import { FormsModule } from '@angular/forms';
 import { FullCalendarModule } from '@fullcalendar/angular';
-import { ErrorComponent } from './components/error/error.component';
-import { CalendarComponent } from './components/calendar/calendar.component';
-import { ContactsComponent } from './components/contacts/contacts.component';
-import { EmailsComponent } from './components/emails/emails.component';
 import {
   SocialLoginModule,
   SocialAuthServiceConfig,
 } from '@abacritt/angularx-social-login';
 import { GoogleLoginProvider } from '@abacritt/angularx-social-login';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { LoaderComponent } from './components/loader/loader.component';
-import { RouterOutlet } from '@angular/router';
-import { BackgroundCanvasComponent } from './components/background-canvas/background-canvas.component';
-import { TextLoaderComponent } from './components/text-loader/text-loader.component';
-import { SvgIaComponent } from './components/ai-body/svg-ia/svg-ia.component';
-import { SvgTalkingComponent } from './components/ai-body/svg-talking/svg-talking.component';
-import { AppService } from './app.service';
-import { AudioWaveComponent } from './components/audio-wave/audio-wave.component';
-import { SleeperScreenComponent } from './components/sleeper-screen/sleeper-screen.component';
-import { InputMsgComponent } from './components/input-msg/input-msg.component';
-import { WelcomeScreenComponent } from './components/welcome-screen/welcome-screen.component';
-import { SvgSpacetravelComponent } from './components/toFix/svg-spacetravel/svg-spacetravel.component';
-import { RealstateBackComponent } from './components/realstate-back/realstate-back.component';
 
 @NgModule({
   declarations: [
@@ -41,21 +32,12 @@ import { RealstateBackComponent } from './components/realstate-back/realstate-ba
     FooterBarComponent,
     MessageAreaComponent,
     AiBodyComponent,
-    ErrorComponent,
-    CalendarComponent,
-    ContactsComponent,
-    EmailsComponent,
-    LoaderComponent,
     BackgroundCanvasComponent,
     TextLoaderComponent,
     SvgIaComponent,
     SvgTalkingComponent,
     AudioWaveComponent,
-    SleeperScreenComponent,
-    InputMsgComponent,
     WelcomeScreenComponent,
-    SvgSpacetravelComponent,
-    RealstateBackComponent,
   ],
   imports: [
     BrowserModule,
